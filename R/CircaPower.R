fc1eb1c37495c6ffbf885479ce22941ef55d8ff2
xiaoguang1988 <zhuo@ufl.edu>
2021-10-08 10:12:13 -0400

    skeleton of CircaPower

---
+++ R/CircaPower.R
@@ -0,0 +1,93 @@
+##' Analytical circadian power calculation based on F-statistics in the cosiner model.
+##'
+##' Analytically calculate power of omics circaidan data under indepednent Normal error assumption using F-statistics.
+##' @title Analytical circadian power calculation based on F-statistics
+##' @param n Sample size.
+##' @param A Amplitude of the sinusoidal curve: \eqn{A * sin(2\pi/period * (phi + cts)) + M}.
+##' @param sigma Standard deviation \eqn{\sigma} in the independent Normal error term \eqn{N(0,\sigma)}.
+##' @param phi Phase shift of the sinusoidal curve. Default is 0.
+##' @param period Period of the sinusoidal curve. Default is 24.
+##' @param cts Circadian times of samples.
+##' @param alpha Type I error control. Default is 0.05.
+##' @return Statistical power calculated from a closed-form formula based on F-statistics.
+##' @author Wei Zong, Zhiguang Huo
+##' @export
+##' @examples
+##' CircaPower(n=NULL, power=0.5908748, r=2.38, phi=0, period = 24, cts=NULL, alpha = 0.001)
+##' 
+##' n = 100
+##' cts = seq(0,24,length.out = n+1)[-1]
+##' A = 1
+##' sigma = 1
+##' phi = 0
+##' CircaPower(n, A, sigma, phi=0, period = 24, cts=cts, alpha = 0.05)
+
+
+
+CircaPower = function(n=NULL, power=NULL, r=NULL, phi=0, period = 24, cts=NULL, ct_estimation=TRUE, alpha = 0.05){
+  
+  if (sum(sapply(list(n, r, power, alpha), is.null)) != 1){
+    stop("exactly one of n, r, power, and alpha must be NULL")		
+  } 
+  
+  w = 2*pi / period
+  
+  if(is.null(cts)){
+    message("No circadian time is specified, evenly-spaced circadian time design will be used.")
+    d = 0.5
+    
+  }else if(is.null(n)){
+    message("The sample size n unspecified, Kernal density method will be used to estimate the distribution of circadian time.")
+    d = sum(sin(w*(cts+phi))^2)/length(cts) #E[d]
+    
+  }else if(!is.null(n) & length(cts) == n){
+    message("The sample size n is the same as the number of circadian time points, which will be used directly.")
+    d = sum(sin(w*(cts+phi))^2)/n
+    
+  }else if(!is.null(n) & length(cts) != n){
+    if(ct_estimation == TRUE){
+      message("The sample size n is different from the number of circadian time points. 
+              Expected samplling design factor is estimated from the circadian time points provided")
+      d = sum(sin(w*(cts+phi))^2)/length(cts) #E[d]
+    }else{
+      message("The sample size n is different from the number of circadian time points. 
+              Circadian time points of size n will be draw from the Kernel density estimated from the circadian time points provided")
+      cts = cts %% period #fold cts into one cycle
+      
+      rep.cts = c(cts-period, cts, cts+period)#expand cts to 3 periods
+      dens = density(rep.cts, n=3e+5, from = -period, to = 2*period)
+      x.dens = dens$x[dens$x>0 & dens$x<period]
+      y.dens = dens$y[dens$x>0 & dens$x<period]
+      
+      sample_cts = sample(x.dens, n, prob = y.dens)
+      d = sum(sin(w*(sample_cts+phi))^2)/length(sample_cts)
+    }
+  }else {
+    stop("internal error 1, please report this bug at https://github.com/circaPower/CircaPower/issues")
+  }
+  
+  p.body = quote({
+    df2 = n - k - 1
+    ncp = r^2 * n * d
+    Z = qf(1 - alpha, df1, df2, ncp = 0)
+    1 - pf(Z, df1, df2, ncp = ncp)
+  })
+  
+  k = 2
+  df1 = k
+  
+  if(is.null(power)){
+    power = eval(p.body)
+  }else if(is.null(n)){
+    n = round(uniroot(function(n) eval(p.body) - power, c(4,1e+9))$root)
+  }else if(is.null(r)){
+    r = uniroot(function(r) eval(p.body) - power, c(1e-10,1e+5))$root
+  }else if (is.null(alpha))
+    alpha = uniroot(function(alpha) eval(p.body) - power, c(1e-10, 1 - 1e-10))$root
+  else stop("internal error 2, please report this bug at https://github.com/circaPower/CircaPower/issues")
+
+  
+  
+  structure(list(n = n, r = r, power = power, alpha = alpha, cts=cts))
+}
+
