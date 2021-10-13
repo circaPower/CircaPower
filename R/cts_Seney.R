0b1367265c6d8fde2129ca1edea738ac9687f58c
wei zong <weizong0527@gmail.com>
2021-10-13 10:45:41 -0400

    add observed cts from human postmortem studies

---
+++ R/cts_Seney.R
@@ -0,0 +1,15 @@
+#' Time of death vector from the human post-mortem brain transcriptomic study on dorsolateral prefrontal cortex.
+#'
+#' @title Dorsolateral Prefrontal Cortex Data (control subjects) Time of Death
+#'
+#' @usage
+#' data(cts_Seney)
+#'
+#' @format A time of death vector.
+#'
+#'
+#' @references
+#' Seney, M. L., Cahill, K., Enwright, J. F., Logan, R. W., Huo, Z., Zong, W., ... & McClung, C. A. (2019). Diurnal rhythms in gene expression in the prefrontal cortex in schizophrenia. Nature communications, 10(1), 1-11.
+#'
+#'
+"cts_Seney"
