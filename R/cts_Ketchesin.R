d73a0840ea4b983ee689b52399da9c7cc794fa0d
wei zong <weizong0527@gmail.com>
2021-10-17 16:37:14 -0400

    human data description

---
+++ R/cts_Ketchesin.R
@@ -0,0 +1,15 @@
+#' Time of death vector from the human post-mortem brain transcriptomic study on the dorsal and ventral straitum.
+#'
+#' @title Dorsal and Ventral Straitum Data (control subjects) Time of Death
+#'
+#' @usage
+#' data(cts_Ketchesin)
+#'
+#' @format A time of death vector.
+#'
+#'
+#' @references
+#' Ketchesin, K. D., Zong, W., Hildebrand, M. A., Seney, M. L., Cahill, K. M., Scott, M. R., ... & McClung, C. A. (2021). Diurnal rhythms across the human dorsal and ventral striatum. Proceedings of the National Academy of Sciences, 118(2).
+#'
+#'
+"cts_Ketchesin"
